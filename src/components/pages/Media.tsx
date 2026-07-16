76b85cc8283b7ac67fe575dd6422bdcaf9e166f3
sprasadik2010 <sprasadik@gmail.com>
2026-07-16 20:04:22 +0530

    err fix

--- src/components/pages/Media.tsx
+++ src/components/pages/Media.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { mediaImages } from '../../data';
 
 const Media = () => {
--- src/components/pages/OurService.tsx
+++ src/components/pages/OurService.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { services } from '../../data';
 
 const OurService = () => {
