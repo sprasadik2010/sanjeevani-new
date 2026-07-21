23926e485ebf315981f9a21fc4582b251c9bce89
sprasadik2010 <sprasadik@gmail.com>
2026-07-21 20:21:25 +0530

    Social media changes

--- src/components/common/StickyActions.tsx
+++ src/components/common/StickyActions.tsx
@@ -42,11 +42,12 @@ const socialLinks: SocialLink[] = [
     id: 'twitter',
     name: 'Twitter / X',
     url: 'https://twitter.com',
-    icon: 'fab fa-x-twitter',
+    icon: 'fab fa-twitter',
     bgColor: 'bg-[#0f1419]',
     hoverBg: 'hover:bg-black',
     shadowColor: 'shadow-gray-700/30',
   },
+
   {
     id: 'linkedin',
     name: 'LinkedIn',
@@ -106,7 +107,14 @@ const StickyActions: React.FC = () => {
                 `}
                 aria-label={`Visit our ${social.name}`}
               >
-                <i className={`${social.icon} text-lg sm:text-xl transition-transform duration-300 group-hover:rotate-6`}></i>
+                {social.id === 'twitter' ? (
+                  <svg className="w-4 h-4 sm:w-5 sm:h-5 fill-current text-white transition-transform duration-300 group-hover:rotate-6" viewBox="0 0 24 24">
+                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+                  </svg>
+                ) : (
+                  <i className={`${social.icon} text-lg sm:text-xl transition-transform duration-300 group-hover:rotate-6`}></i>
+                )}
+
               </a>
             </div>
           );
